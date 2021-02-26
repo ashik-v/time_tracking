2dc2767db775a22d95fe1500925855d57d4183f9
Ashik Varghese <ashikv@nulogy.com>
2021-02-26 13:03:11 -0500

    added commands

--- time_tracking.rb
+++ time_tracking.rb
@@ -1,1 +1,27 @@
-puts 3 + 5 * 50
+# projects
+# select project
+# start timer
+# stop timer
+# record time to project
+# update timer
+
+
+# c = create, l = list, d = delete, s = select
+# b = begin timer, e = end timer
+projects = []
+
+while true
+  puts "What is the command?\nc = create, l = list, d = delete\n s = select b = begin timer e = end timer"
+  command = gets.chomp
+  case command
+  when /^c/
+    puts "What is the project name?"
+    project_name = gets.chomp
+    projects << project_name
+  when /^l/
+    puts projects
+  end
+end
+
+
+
