d67ad21d87e0b5b65987a7bb6805852bd2f2d40e
Ashik Varghese <ashikv@nulogy.com>
2021-03-16 16:20:11 -0400

    added class and main method

--- time_tracking.rb
+++ time_tracking.rb
@@ -12,50 +12,58 @@ Project = Struct.new(:name, :minutes, :start_time) do
   end
 end
 
-store = YAML::Store.new "projects.yaml"
-projects = nil
-store.transaction do
-  projects = store["Projects"] || []
+
+
+class TimeTracking
+  def main
+    store = YAML::Store.new "projects.yaml"
+    projects = nil
+    store.transaction do
+      projects = store["Projects"] || []
+    end
+    
+    while true
+      puts format("%-15s | %10s | %10s", "Project Name", "Duration", "Start Time")
+      projects.each do |project|
+        puts format("%-15s | %10s | %10s", project.name, project.minutes, project.start_time || "-")
+      end
+      puts "What is the command?\nc = create, l = list, d = delete\n s = set duration b = begin timer e = end timer"
+      command = gets.chomp
+      case command
+      when /^c/
+        puts "What is the project name?"
+        project_name = gets.chomp
+        projects << Project.new(project_name, 0)
+      when /^l/
+        puts projects
+      when /^d/
+        puts "Which project would you like to delete?"
+        project_name = gets.chomp
+        projects.delete_if { |project| project.name == project_name }
+      when /^b/
+        puts "Which project would you like to start a timer for?"
+        project_name = gets.chomp
+        project = projects.find { |project| project.name == project_name }
+        project.start_time = Time.now
+      when /^e/
+        puts "What project would you like to end timer for?"
+        project_name = gets.chomp
+        project = projects.find { |project| project.name == project_name }
+        project.minutes = (Time.now - project.start_time)/60
+        project.start_time = nil
+      when /^s/
+        puts "What project would you like to edit timer for?"
+        project_name = gets.chomp
+        project = projects.find { |project| project.name == project_name }
+        puts "What is the updated duration?"
+        project.minutes = gets.chomp.to_i
+        project.start_time = nil
+      end
+      store.transaction do
+        store["Projects"] = projects
+      end
+    end   
+  end
 end
 
-while true
-  puts format("%-15s | %10s | %10s", "Project Name", "Duration", "Start Time")
-  projects.each do |project|
-    puts format("%-15s | %10s | %10s", project.name, project.minutes, project.start_time || "-")
-  end
-  puts "What is the command?\nc = create, l = list, d = delete\n s = set duration b = begin timer e = end timer"
-  command = gets.chomp
-  case command
-  when /^c/
-    puts "What is the project name?"
-    project_name = gets.chomp
-    projects << Project.new(project_name, 0)
-  when /^l/
-    puts projects
-  when /^d/
-    puts "Which project would you like to delete?"
-    project_name = gets.chomp
-    projects.delete_if { |project| project.name == project_name }
-  when /^b/
-    puts "Which project would you like to start a timer for?"
-    project_name = gets.chomp
-    project = projects.find { |project| project.name == project_name }
-    project.start_time = Time.now
-  when /^e/
-    puts "What project would you like to end timer for?"
-    project_name = gets.chomp
-    project = projects.find { |project| project.name == project_name }
-    project.minutes = (Time.now - project.start_time)/60
-    project.start_time = nil
-  when /^s/
-    puts "What project would you like to edit timer for?"
-    project_name = gets.chomp
-    project = projects.find { |project| project.name == project_name }
-    puts "What is the updated duration?"
-    project.minutes = gets.chomp.to_i
-    project.start_time = nil
-  end
-  store.transaction do
-    store["Projects"] = projects
-  end
-end
+TimeTracking.new.main
