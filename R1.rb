6f49d19153fbf6313e9a20b99b76b7a3bced6851
Ashik Varghese <ashikv@nulogy.com>
2021-03-16 21:33:41 -0400

    added yaml, project class

--- R1.rb
+++ R1.rb
@@ -1,4 +1,16 @@
+require 'yaml/store'
+
+Project = Struct.new(:name) do
+  def to_s
+    name
+  end
+end
+
+store = YAML::Store.new "scrap_projects.yaml"
 projects = []
+store.transaction do
+  projects = store["Projects"] || []
+end
 
 while true
   puts "Enter Command: \n c: create d: delete l: list"
@@ -8,12 +20,15 @@ while true
   when /^c/
     puts "Enter project name"
     project_name = gets.chomp
-    projects << project_name
+    projects << Project.new(project_name)
   when /^d/
     puts "Enter project name"
     project_name = gets.chomp
-    projects.delete(project_name)
+    projects.delete_if {|project| project.name == project_name}
   when /^l/
     puts projects
+  end
+  store.transaction do
+    store["Projects"] = projects
   end
 end
