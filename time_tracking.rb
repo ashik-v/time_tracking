7ed138e0344e2c208864153c0b3e83e6bb6501ad
Ashik Varghese <ashikv@nulogy.com>
2021-03-28 18:03:34 -0400

    updated formatting, updated variable names to be more clear

--- time_tracking.rb
+++ time_tracking.rb
@@ -6,15 +6,15 @@
 # b = begin timer, e = end timer
 require_relative "project_repo"
 
-Project = Struct.new(:name, :minutes, :start_time)
+Project = Struct.new(:name, :minutes, :last_started_at, :display_time)
 
 class TimeTracking
   def main
     projects = ProjectRepo.load_projects
     while true
-      puts format("%-15s | %10s | %10s", "Project Name", "Duration", "Start Time")
+      puts format("|%-15s | %-5s | %-26s|", "Project Name", "Timer", "Last Started At")
       projects.each do |project|
-        puts format("%-15s | %10s | %10s", project.name, project.minutes, project.start_time || "-")
+        puts format("|%-15s | %-5s | %-26s|", project.name, project.display_time || "-", project.last_started_at || "-")
       end
       puts "What is the command?\nc = create, d = delete\n s = set duration b = begin timer e = end timer"
       command = gets.chomp
@@ -31,20 +31,22 @@ class TimeTracking
         puts "Which project would you like to start a timer for?"
         project_name = gets.chomp
         project = projects.find { |project| project.name == project_name }
-        project.start_time = Time.now
+        project.last_started_at = Time.now
       when /^e/
         puts "What project would you like to end timer for?"
         project_name = gets.chomp
         project = projects.find { |project| project.name == project_name }
-        project.minutes = ((Time.now - project.start_time)/60).to_i
-        project.start_time = nil
+        project.minutes += ((Time.now - project.last_started_at)/60).to_i
+        project.last_started_at = nil
+        project.display_time = format("%02d:%02d",(project.minutes.to_i/60).to_i, (project.minutes.to_i%60).to_i)
       when /^s/
-        puts "What project would you like to edit timer for?"
+        puts "What project would you like to edit minutes for?"
         project_name = gets.chomp
         project = projects.find { |project| project.name == project_name }
-        puts "What is the updated duration?"
+        puts "What are the updated minutes?"
         project.minutes = gets.chomp.to_i
-        project.start_time = nil
+        project.last_started_at = nil
+        project.display_time = format("%02d:%02d",(project.minutes.to_i/60).to_i, (project.minutes.to_i%60).to_i)
       end
       ProjectRepo.save_projects(projects)
     end   
