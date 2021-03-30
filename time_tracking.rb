8716cfe469582bd96e0634c03b123692d70f46f8
Ashik Varghese <ashikv@nulogy.com>
2021-03-30 16:52:00 -0400

    extracted display_prompt and handle_command method

--- time_tracking.rb
+++ time_tracking.rb
@@ -13,48 +13,56 @@ Project = Struct.new(:name, :minutes, :last_started_at) do
 end
 
 class TimeTracking
+  def main
+    while true
+      display_projects
+      display_prompt
+      command = gets.chomp
+      handle_command(command)
+      ProjectRepo.save_projects(projects)
+    end   
+  end
+
   def display_projects
     puts format("|%-15s | %-5s | %-26s|", "Project Name", "Timer", "Last Started At")
     projects.each do |project|
       puts format("|%-15s | %-5s | %-26s|", project.name, project.display_time || "-", project.last_started_at || "-")
     end
+  end
+
+  def display_prompt
     puts "What is the command?\nc = create, d = delete\n s = set duration b = begin timer e = end timer"
   end
 
-  def main
-    while true
-      display_projects
-      command = gets.chomp
-      case command
-      when /^c/
-        puts "What is the project name?"
-        project_name = gets.chomp
-        projects << Project.new(project_name, 0)
-      when /^d/
-        puts "Which project would you like to delete?"
-        project_name = gets.chomp
-        projects.delete_if { |project| project.name == project_name }
-      when /^b/
-        puts "Which project would you like to start a timer for?"
-        project_name = gets.chomp
-        project = projects.find { |project| project.name == project_name }
-        project.last_started_at = Time.now
-      when /^e/
-        puts "What project would you like to end timer for?"
-        project_name = gets.chomp
-        project = projects.find { |project| project.name == project_name }
-        project.minutes += ((Time.now - project.last_started_at)/60).to_i
-        project.last_started_at = nil
-      when /^s/
-        puts "What project would you like to edit minutes for?"
-        project_name = gets.chomp
-        project = projects.find { |project| project.name == project_name }
-        puts "What are the updated minutes?"
-        project.minutes = gets.chomp.to_i
-        project.last_started_at = nil
-      end
-      ProjectRepo.save_projects(projects)
-    end   
+  def handle_command (command)
+    case command
+    when /^c/
+      puts "What is the project name?"
+      project_name = gets.chomp
+      projects << Project.new(project_name, 0)
+    when /^d/
+      puts "Which project would you like to delete?"
+      project_name = gets.chomp
+      projects.delete_if { |project| project.name == project_name }
+    when /^b/
+      puts "Which project would you like to start a timer for?"
+      project_name = gets.chomp
+      project = projects.find { |project| project.name == project_name }
+      project.last_started_at = Time.now
+    when /^e/
+      puts "What project would you like to end timer for?"
+      project_name = gets.chomp
+      project = projects.find { |project| project.name == project_name }
+      project.minutes += ((Time.now - project.last_started_at)/60).to_i
+      project.last_started_at = nil
+    when /^s/
+      puts "What project would you like to edit minutes for?"
+      project_name = gets.chomp
+      project = projects.find { |project| project.name == project_name }
+      puts "What are the updated minutes?"
+      project.minutes = gets.chomp.to_i
+      project.last_started_at = nil
+    end
   end
 
   def projects
