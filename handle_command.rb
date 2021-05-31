81a6b9790482d4d7e1f65afaae4b11d6ef96acbc
Ashik Varghese <ashikv@nulogy.com>
2021-05-31 13:24:12 -0700

    extracted classes

--- handle_command.rb
+++ handle_command.rb
@@ -1,58 +1,62 @@
-class HandleCommand
-  attr_reader :projects
-
-  def initialize(projects)
-    @projects = projects
-  end
-
+class HandleCommand < Struct.new(:projects)
   def handle_command(command)
     case command
     when /^c/
-      create_project
+      CreateProject.new(projects).run
     when /^d/
-      delete_project
+      DeleteProject.new(projects).run
     when /^b/
-      start_timer
+      StartTimer.new(projects).run
     when /^e/
-      end_timer
+      EndTimer.new(projects).run
     when /^s/
-      edit_timer
+      EditTimer.new(projects).run
     end
   end
 
-  def create_project
-    puts "What is the project name?"
-    project_name = gets.chomp
-    projects << Project.new(project_name, 0)
+  class CreateProject < Struct.new(:projects)
+    def run
+      puts "What is the project name?"
+      project_name = gets.chomp
+      projects << Project.new(project_name, 0)
+    end
   end
 
-  def delete_project
-    puts "Which project would you like to delete?"
-    project_name = gets.chomp
-    projects.delete_if { |project| project.name == project_name }
+  class DeleteProject < Struct.new(:projects)
+    def run
+      puts "Which project would you like to delete?"
+      project_name = gets.chomp
+      projects.delete_if { |project| project.name == project_name }
+    end
   end
 
-  def start_timer
-    puts "Which project would you like to start a timer for?"
-    project_name = gets.chomp
-    project = projects.find { |project| project.name == project_name }
-    project.last_started_at = Time.now
+  class StartTimer < Struct.new(:projects)
+    def run
+      puts "Which project would you like to start a timer for?"
+      project_name = gets.chomp
+      project = projects.find { |project| project.name == project_name }
+      project.last_started_at = Time.now
+    end
   end
 
-  def end_timer
-    puts "What project would you like to end timer for?"
-    project_name = gets.chomp
-    project = projects.find { |project| project.name == project_name }
-    project.minutes += ((Time.now - project.last_started_at) / 60).to_i
-    project.last_started_at = nil
+  class EndTimer < Struct.new(:projects)
+    def run
+      puts "What project would you like to end timer for?"
+      project_name = gets.chomp
+      project = projects.find { |project| project.name == project_name }
+      project.minutes += ((Time.now - project.last_started_at) / 60).to_i
+      project.last_started_at = nil
+    end
   end
 
-  def edit_timer
-    puts "What project would you like to edit minutes for?"
-    project_name = gets.chomp
-    project = projects.find { |project| project.name == project_name }
-    puts "What are the updated minutes?"
-    project.minutes = gets.chomp.to_i
-    project.last_started_at = nil
+  class EditTimer < Struct.new(:projects)
+    def run
+      puts "What project would you like to edit minutes for?"
+      project_name = gets.chomp
+      project = projects.find { |project| project.name == project_name }
+      puts "What are the updated minutes?"
+      project.minutes = gets.chomp.to_i
+      project.last_started_at = nil
+    end
   end
 end
