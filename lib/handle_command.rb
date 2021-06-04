a2228d38773b4edc350d77e0b5c34cf7bffbcae5
Ashik Varghese <ashikv@nulogy.com>
2021-06-04 11:30:11 -0700

    Extract find_command_class method

--- lib/handle_command.rb
+++ lib/handle_command.rb
@@ -1,19 +1,18 @@
 class HandleCommand < Struct.new(:projects)
   def handle_command(command)
+    command_class = find_command_class(command)
+    return puts("\nERROR: Invalid Command - enter valid command\n\n") unless command_class
+
+    command_class.new(projects).run
+  end
+
+  def find_command_class(command)
     case command
-    when /^c/
-      Commands::CreateProject
-    when /^d/
-      Commands::DeleteProject
-    when /^b/
-      Commands::StartTimer
-    when /^e/
-      Commands::EndTimer
-    when /^s/
-      Commands::EditTimer
-    else
-      puts "Invalid Command - enter command"
-      return
-    end.new(projects).run
+    when /^c/ then Commands::CreateProject
+    when /^d/ then Commands::DeleteProject
+    when /^b/ then Commands::StartTimer
+    when /^e/ then Commands::EndTimer
+    when /^s/ then Commands::EditTimer
+    end
   end
 end
