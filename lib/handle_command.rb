6b5fad0d9354a9ff68814e21022d62c4e76554f1
Ashik Varghese <ashikv@nulogy.com>
2021-06-16 08:53:23 -0700

    Refactor: Improve names

--- lib/handle_command.rb
+++ lib/handle_command.rb
@@ -8,18 +8,18 @@ class HandleCommand < Struct.new(:projects)
   }
 
   def self.command_header
-    COMMANDS.map { |command, command_class| "#{command} - #{command_class.description}" }
+    COMMANDS.map { |key, command| "#{key} - #{command.description}" }
   end
 
-  def handle_command(command)
-    command_class = find_command_class(command)
-    return puts("\nERROR: Invalid Command - enter valid command\n\n") unless command_class
+  def handle_command(user_input)
+    command = find_command(user_input)
+    return puts("\nERROR: Invalid Command - enter valid command\n\n") unless command
 
-    command_class.new(projects).run
+    command.new(projects).run
   end
 
-  def find_command_class(user_input)
-    command = user_input[0]
-    COMMANDS[command]
+  def find_command(user_input)
+    key = user_input[0]
+    COMMANDS[key]
   end
 end
