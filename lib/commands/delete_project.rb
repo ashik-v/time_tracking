a1564044ab3b0f3f32ff3322789c5fd8d763dbee
Ashik Varghese <ashikv@nulogy.com>
2021-06-25 08:40:02 -0700

    Refactor: Extract get_string method

--- lib/commands/delete_project.rb
+++ lib/commands/delete_project.rb
@@ -2,7 +2,7 @@ module Commands
   class DeleteProject < Struct.new(:projects)
     def run
       print "Which project would you like to delete? "
-      project_name = gets.chomp
+      project_name = get_string.chomp
       projects.delete_if { |p| p.name == project_name }
     end
   end
--- lib/commands/end_timer.rb
+++ lib/commands/end_timer.rb
@@ -2,7 +2,7 @@ module Commands
   class EndTimer < Struct.new(:projects)
     def run
       print "What project would you like to end timer for? "
-      project_name = gets.chomp
+      project_name = get_string.chomp
       project = projects.find { |p| p.name == project_name }
       project.minutes += ((Time.now - project.last_started_at) / 60).to_i
       project.last_started_at = nil
